f8d063cdd1c604de2dd589d371b8aad599dc319a
packo <packo@10.168.2.136>
2017-12-21 12:33:01 -0700

    panel administrativo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ImageListComponent } from './gallery/image-list/image-list.component';
+import { ImageComponent } from './gallery/image-list/image.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    GalleryComponent,
+    ImageListComponent,
+    ImageComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/gallery/image-list/image-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ng-image-list',
+  templateUrl: './image-list.component.html',
+  styles: []
+})
+export class ImageListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
